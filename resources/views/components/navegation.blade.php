c6f9518e739147dda4278e5531a613276d36e9f6
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2023-12-18 15:05:30 -0600

    add diseño de la vista principal

---
+++ resources/views/components/navegation.blade.php
@@ -0,0 +1,26 @@
+<nav class="flex justify-between h-16 border bg-white">
+        <div class="w-1/4 flex items-center ml-4">
+            <img 
+                src="https://web.sabbi.app/images/logo/SABBI2.png"  alt=""
+                class="h-11"
+            >
+        </div>
+
+        <div class="w-1/4 flex justify-around h-16 items-center mr-4">
+            <div class="mx-3">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+            </div>
+
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v25.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm0 96c61.9 0 112 50.1 112 112v25.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V208c0-61.9 50.1-112 112-112zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"/></svg>
+            </div>
+
+            <div class="flex items-center">
+                <img 
+                    class="w-[200px] h-14"
+                    src="https://landing.academic.lat/assets/image/a32bde.jpeg" alt=""
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
+            </div>
+        </div>
+    </nav>
